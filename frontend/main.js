2bd9f55839963d1dda76d1dc7132f7cd2762a5d2
don-guapo <jditprojects@outlook.com>
2025-04-28 17:56:27 -0400

    frontend edits

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,19 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = '';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json()
-    }).then(resonse=>{
-        console.log("Website called function API");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
+    fetch(functionUrl)  // <-- use the variable imported from config.js
+        .then(response => response.json())
+        .then(response => {
+            console.log("Website called function API");
+            count = response.count;
+            document.getElementById("counter").innerText = count;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
     return count;
 }
