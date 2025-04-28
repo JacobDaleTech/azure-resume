55b4df2e3eab38fca41c469613c9a965e5f6fad3
don-guapo <jditprojects@outlook.com>
2025-04-28 12:39:10 -0400

    added frontend and backend workflows

--- terraform/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg-azureresume" {
-  name     = "rg-azureresume"
-  location = "East US"
-}
--- terraform/modules/resource_group/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg_azureresume" {
-  name     = "rg-azureresume"
-  location = "East US"
-}
